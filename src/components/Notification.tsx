b6aa72399be54c3bf2e4b652689df0dfd0c082a3
anhduc2901 <20520890@gm.uit.edu.vn>
2023-11-14 09:59:45 +0700

    Hello customers

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,9 +1,22 @@
+import { useSession } from 'next-auth/react';
 import React from 'react'
+import Hello from './Hello';
+
 
 const Notification = () => {
+
   return (
-    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'>
-      Free delivery for all orders over $50. Order your food now !
+    <div className='h-12 bg-red-500 text-white px-4 flex items-center justify-around text-center text-sm md:text-base cursor-pointer'>
+      <div className='flex-1'>
+        <Hello />
+      </div>
+      <div className='flex-1'>
+        Free delivery for all orders over $50. Order your food now !
+      </div>
+
+
+
+
     </div>
   )
 }
